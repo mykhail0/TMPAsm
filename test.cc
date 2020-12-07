fa3f31aaacdd272c20e43b2ea896b4d83a4c2252
j.kusmierczyk <j.kusmierczyk@lekseek.com>
2020-12-07 01:04:54 +0100

    working memory

--- test.cc
+++ test.cc
@@ -1,31 +1,42 @@
 #include <array>
 #include <iostream>
 
-template <typename... Ops> struct Program {
-    template <int memSize, typename memType>
-    static constexpr void run(std::array<memType, memSize>& mem);
-};
-template <typename Op, typename... Ops> struct Program<Op, Ops...> {
+template <size_t N, typename Type>
+inline void printMemory(std::array<Type, N>& mem) {
+    std::cout << "-----MEMORY STATE----" << std::endl;
+    for (size_t i = 0; i < N; i++) {
+        std::cout << mem[i] << std::endl;
+    }
+    std::cout << "---------" << std::endl;
+}
+
+template <typename Op, typename... Ops> struct Program {
     //  wywołanie rekurencyjne
-    template <int memSize, typename memType>
+    template <size_t memSize, typename memType>
     static constexpr void run(std::array<memType, memSize>& mem) {
-        std::cout << "rekurencja" << std::endl;
+        Op::template run<memSize, memType>(mem);
+        printMemory<memSize, memType>(mem);
         Program<Ops...>::run(mem);
     };
 };
 template <typename Op> struct Program<Op> {
     //  wywołanie bazowe
-    template <int memSize, typename memType>
+    template <size_t memSize, typename memType>
     static constexpr void run(std::array<memType, memSize>& mem) {
-        std::cout << "koniec rekurencji " << mem[0] << std::endl;
-        mem[0] = 4;
-        std::cout << "koniec rekurencji " << mem[0] << std::endl;
+        Op::template run<memSize, memType>(mem);
+        printMemory<memSize, memType>(mem);
     };
 };
 
-struct xd {};
+// Przykładowa operacja
+template <int a, int b> struct TestOp {
+    template <size_t memSize, typename memType>
+    static constexpr void run(std::array<memType, memSize>& mem) {
+        mem[a] = b;
+    }
+};
 
-template <int N, typename Type> struct Computer {
+template <size_t N, typename Type> struct Computer {
     template <typename T> static constexpr std::array<Type, N> boot() {
         std::array<Type, N> memory{};
         T::template run<N, Type>(memory);
@@ -33,4 +44,7 @@ template <int N, typename Type> struct Computer {
     }
 };
 
-int main() { Computer<10, int>::boot<Program<xd>>(); }
+int main() {
+    Computer<10, int>::boot<
+        Program<TestOp<0, 3>, TestOp<1, 2>, TestOp<0, 2>, TestOp<8, 2137>>>();
+}
