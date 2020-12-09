38c5246fadb45267e7c3bc7ba3bcb9d38dbc97c4
j.kusmierczyk <j.kusmierczyk@lekseek.com>
2020-12-09 16:55:32 +0100

    fix formatting

--- src/computer.h
+++ src/computer.h
@@ -5,8 +5,8 @@
 #include <iostream>
 
 //---------------DEBUG----------------------
-template <size_t N, typename Type>
-inline void printMemory(std::array<Type, N>& mem) {
+template<size_t N, typename Type>
+inline void printMemory(std::array <Type, N> &mem) {
     std::cout << "-----MEMORY STATE----" << std::endl;
     for (size_t i = 0; i < N; i++) {
         std::cout << mem[i] << std::endl;
@@ -16,73 +16,80 @@ inline void printMemory(std::array<Type, N>& mem) {
 
 //-------------OPERATIONS---------------------------
 
-enum OpType { TEST, LABEL, JUMP };
+enum OpType {
+    TEST, LABEL, JUMP
+};
 
 // Przykładowa operacja
-template <int a, int b> struct TestOp {
-    template <size_t memSize, typename memType>
-    static constexpr void execute(std::array<memType, memSize>& mem, bool& ZF,
-                                  bool& SF) {
+template<int a, int b>
+struct TestOp {
+    template<size_t memSize, typename memType>
+    static constexpr void execute(std::array <memType, memSize> &mem, bool &ZF, bool &SF) {
         mem[a] = b;
     }
+
     static constexpr OpType type = TEST;
 };
 
-template <typename Label, typename Program> struct LabelHolder {
+template<typename Label, typename Program>
+struct LabelHolder {
     using program = Program;
     using label = Label;
 };
 
-template <uint64_t Id> struct Label {
+template<uint64_t Id>
+struct Label {
 
-    template <size_t memSize, typename memType>
-    static constexpr void execute(std::array<memType, memSize>& mem, bool& ZF,
-                                  bool& SF) {}
+    template<size_t memSize, typename memType>
+    static constexpr void execute(std::array <memType, memSize> &mem, bool &ZF, bool &SF) {}
+
     static constexpr OpType type = LABEL;
     static constexpr uint64_t id = Id;
 };
-template <uint64_t Id> struct Jump {
 
-    template <size_t memSize, typename memType, typename labels>
-    static constexpr void execute(std::array<memType, memSize>& mem, bool& ZF,
-                                  bool& SF) {
-        labels::template find_and_run<memSize, memType, labels>(Id, mem, ZF,
-                                                                SF);
+template<uint64_t Id>
+struct Jump {
+
+    template<size_t memSize, typename memType, typename labels>
+    static constexpr void execute(std::array <memType, memSize> &mem, bool &ZF, bool &SF) {
+        labels::template find_and_run<memSize, memType, labels>(Id, mem, ZF, SF);
     }
+
     static constexpr OpType type = JUMP;
     static constexpr uint64_t id = Id;
 };
 
 //----------------HANDLING LABELS-------------------
-template <typename... Labels> struct LabelList;
+template<typename... Labels>
+struct LabelList;
 
 // Create list
 
 // recursion if label
-template <template <typename...> class Program, typename... Ops,
-          typename... Labels, uint64_t n>
+template<template<typename...> class Program, typename... Ops,
+        typename... Labels, uint64_t n>
 struct LabelList<Program<Label<n>, Ops...>, Labels...> {
     using result =
-        typename LabelList<Program<Ops...>, Labels...,
-                           LabelHolder<Label<n>, Program<Ops...>>>::result;
+    typename LabelList<Program<Ops...>, Labels...,
+            LabelHolder<Label<n>, Program<Ops...>>>::result;
 };
 
 // recursion otherwise
-template <template <typename...> class Program, typename... Ops, typename Op,
-          typename... Labels>
+template<template<typename...> class Program, typename... Ops, typename Op,
+        typename... Labels>
 struct LabelList<Program<Op, Ops...>, Labels...> {
     using result = typename LabelList<Program<Ops...>, Labels...>::result;
 };
 
 // base case if label
-template <template <typename...> class Program, typename... Labels, uint64_t n>
+template<template<typename...> class Program, typename... Labels, uint64_t n>
 struct LabelList<Program<Label<n>>, Labels...> {
     using result =
-        LabelList<Program<>, Labels..., LabelHolder<Label<n>, Program<>>>;
+    LabelList<Program<>, Labels..., LabelHolder<Label<n>, Program<>>>;
 };
 
 // base case otherwise
-template <template <typename...> class Program, typename Op, typename... Labels>
+template<template<typename...> class Program, typename Op, typename... Labels>
 struct LabelList<Program<Op>, Labels...> {
     using result = LabelList<Program<>, Labels...>;
 };
@@ -90,30 +97,30 @@ struct LabelList<Program<Op>, Labels...> {
 // Handle Jump operations
 
 // recursion
-template <template <typename...> class Program, typename Label,
-          typename... Labels>
+template<template<typename...> class Program, typename Label,
+        typename... Labels>
 struct LabelList<Program<>, Label, Labels...> {
-    template <size_t memSize, typename memType, typename labels>
+    template<size_t memSize, typename memType, typename labels>
     static constexpr void find_and_run(uint64_t id,
-                                       std::array<memType, memSize>& mem,
-                                       bool& ZF, bool& SF) {
+                                       std::array <memType, memSize> &mem,
+                                       bool &ZF, bool &SF) {
         // std::cout << Label::label::type << std::endl;
         if (Label::label::id == id) {
             Label::program::template run<memSize, memType, labels>(mem, ZF, SF);
         } else {
             LabelList<Program<>, Labels...>::template find_and_run<
-                memSize, memType, labels>(mem, ZF, SF);
+                    memSize, memType, labels>(mem, ZF, SF);
         }
     }
 };
 
 // base case
-template <template <typename...> class Program, typename Label>
+template<template<typename...> class Program, typename Label>
 struct LabelList<Program<>, Label> {
-    template <size_t memSize, typename memType, typename labels>
+    template<size_t memSize, typename memType, typename labels>
     static constexpr void find_and_run(uint64_t id,
-                                       std::array<memType, memSize>& mem,
-                                       bool& ZF, bool& SF) {
+                                       std::array <memType, memSize> &mem,
+                                       bool &ZF, bool &SF) {
         // std::cout << Label::label::type << std::endl;
         if (Label::label::id == id) {
             Label::program::template run<memSize, memType, labels>(mem, ZF, SF);
@@ -122,51 +129,53 @@ struct LabelList<Program<>, Label> {
 };
 
 //------------------PROGRAM----------------------
-template <typename... Ops> struct Program {
-    template <size_t memSize, typename memType, typename labels>
-    static constexpr void run(std::array<memType, memSize>& mem, bool& ZF,
-                              bool& SF);
+template<typename... Ops>
+struct Program {
+    template<size_t memSize, typename memType, typename labels>
+    static constexpr void run(std::array <memType, memSize> &mem, bool &ZF, bool &SF);
 };
 
-template <typename Op, typename... Ops> struct Program<Op, Ops...> {
+template<typename Op, typename... Ops>
+struct Program<Op, Ops...> {
     // recursion call
-    template <size_t memSize, typename memType, typename labels>
-    static constexpr void run(std::array<memType, memSize>& mem, bool& ZF,
-                              bool& SF) {
+    template<size_t memSize, typename memType, typename labels>
+    static constexpr void run(std::array <memType, memSize> &mem, bool &ZF, bool &SF) {
         switch (Op::type) {
-        case JUMP:
-            std::cout << "JUMP" << std::endl;
-            Op::template execute<memSize, memType, labels>(mem, ZF, SF);
-            break;
-        default:
-            Op::template execute<memSize, memType>(mem, ZF, SF);
-            printMemory<memSize, memType>(mem);
-            Program<Ops...>::template run<memSize, memType, labels>(mem, ZF,
-                                                                    SF);
-            break;
+            case JUMP:
+                std::cout << "JUMP" << std::endl;
+                Op::template execute<memSize, memType, labels>(mem, ZF, SF);
+                break;
+            default:
+                Op::template execute<memSize, memType>(mem, ZF, SF);
+                printMemory<memSize, memType>(mem);
+                Program<Ops...>::template run<memSize, memType, labels>(mem, ZF, SF);
+                break;
         }
     }
 };
 
-template <typename Op> struct Program<Op> {
+template<typename Op>
+struct Program<Op> {
     // base of recursion
-    template <size_t memSize, typename memType, typename labels>
-    static constexpr void run(std::array<memType, memSize>& mem, bool& ZF,
-                              bool& SF) {
+    template<size_t memSize, typename memType, typename labels>
+    static constexpr void run(std::array <memType, memSize> &mem, bool &ZF, bool &SF) {
         switch (Op::type) {
-        case JUMP:
-            std::cout << "JUMP" << std::endl;
-            Op::template execute<memSize, memType, labels>(mem, ZF, SF);
-            break;
-        default:
-            Op::template execute<memSize, memType>(mem, ZF, SF);
-            printMemory<memSize, memType>(mem);
-            break;
+            case JUMP:
+                std::cout << "JUMP" << std::endl;
+                Op::template execute<memSize, memType, labels>(mem, ZF, SF);
+                break;
+            default:
+                Op::template execute<memSize, memType>(mem, ZF, SF);
+                printMemory<memSize, memType>(mem);
+                break;
         }
     }
 };
 
-template <int N> struct Num { constexpr static int value = N; };
+template<int N>
+struct Num {
+    constexpr static int value = N;
+};
 
 // struct Id {
 //     constexpr static int MAX_LENGTH = 6;
@@ -191,9 +200,11 @@ struct Add {
 };
 */
 
-template <size_t N, typename Type> struct Computer {
-    template <typename T> static constexpr std::array<Type, N> boot() {
-        std::array<Type, N> memory{};
+template<size_t N, typename Type>
+struct Computer {
+    template<typename T>
+    static constexpr std::array <Type, N> boot() {
+        std::array <Type, N> memory{};
         bool ZF = false, SF = false;
         using labels = typename LabelList<T>::result;
         // std::array<uint64_t, n_labels> labels;
